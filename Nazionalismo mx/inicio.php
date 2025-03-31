c87c62c8335bb7f8ceed55bbcebb390bf75e3a9c
angel117-XD <angelrenemarin0@gmail.com>
2025-03-30 22:15:28 -0600

    Add files via upload

---
+++ Nazionalismo mx/inicio.php
@@ -0,0 +1,183 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+
+	<!-- Start WOWSlider.com HEAD section -->
+<link rel="stylesheet" type="text/css" href="engine1/style.css" />
+<script type="text/javascript" src="engine1/jquery.js"></script>
+<!-- End WOWSlider.com HEAD section -->
+
+</head>
+<body>
+
+	<style type="text/css">
+		body{
+			background-color: black;
+			text-align: center;
+			font-size: 30px;
+			color: white;
+		}
+		.manifiesto{
+			text-align: center;
+		    font-size: 50px;
+		}
+		.MNM{
+			text-align: center;
+			font-size: 75px;
+		}
+
+		.navegacion{
+			width: 60%;
+			margin: 30px auto;
+			background: orange;
+		}
+		.navegacion ul{
+			list-style: none;
+		}
+		.menu > li {
+			position: relative;
+			display: inline-block;
+		}
+		.menu >li >a {
+			display: block;
+			padding-left: 20px;
+			padding-right: 20px;
+			padding-top: 15px;
+			padding-bottom: 15px;
+			color: ghostwhite;
+			font-family: 'monospace';
+			text-decoration: none;
+		}
+		.menu li:hover{
+			visibility: visible;
+		}
+		.submenu li a{
+			display: block;
+			padding: 15px;
+			color: #fff;
+			font-family: monospace;
+			text-decoration: none;
+		}
+		.submenu{
+			position: absolute;
+			background: #333333;
+			width: 130px;
+			visibility: hidden;
+			opacity: 0;
+			transition: opacity 1.5x;
+		}
+		.menu li:hover .submenu{
+			visibility: visible;
+			opacity: 1;
+		}
+	</style>
+
+
+	<header>
+		<nav class="navegacion">
+			<ul class="menu">
+				<li><a href="Inicio.php"> inicio </a></li>
+				<li><a href="Nosotros.php"> Nosotros </a></li>
+				<li><a href="#"> . </a>
+				    <ul class="submenu">
+				    	<li><a href="Legos.php">Legos</a></li>
+				    	<li><a href="MegaConstrux.php">Mega Construx</a></li>
+				    </ul>
+				</li>
+				<li><a href="FORMULARIO2 GuerecaMarin.php">Unirme</a></li>
+			</ul>	
+		</nav>
+	</header>
+
+	<label class="MNM">Movimiento Nazionalista Mexicano (M.N.M)</label><br><br>
+
+
+	<!-- Start WOWSlider.com BODY section -->
+<div id="wowslider-container1">
+<div class="ws_images"><ul>
+		<li><img src="data1/images/693_sin_titulo_20250320235254.png" alt="693_sin_titulo_20250320235254" title="693_sin_titulo_20250320235254" id="wows1_0"/></li>
+		<li><img src="data1/images/692_sin_titulo_20250326092951.png" alt="692_sin_titulo_20250326092951" title="692_sin_titulo_20250326092951" id="wows1_1"/></li>
+		<li><img src="data1/images/699_sin_titulo_20250324083516.png" alt="699_sin_titulo_20250324083516" title="699_sin_titulo_20250324083516" id="wows1_2"/></li>
+		<li><img src="data1/images/703_sin_titulo_20250325084526.png" alt="703_sin_titulo_20250325084526" title="703_sin_titulo_20250325084526" id="wows1_3"/></li>
+		<li><img src="data1/images/707_sin_titulo_20250327223117.png" alt="707_sin_titulo_20250327223117" title="707_sin_titulo_20250327223117" id="wows1_4"/></li>
+		<li><img src="data1/images/707_sin_titulo_20250327224910.png" alt="707_sin_titulo_20250327224910" title="707_sin_titulo_20250327224910" id="wows1_5"/></li>
+		<li><img src="data1/images/lun_24_03_2025_19_17_47.png" alt="lun_24_03_2025_19_17_47" title="lun_24_03_2025_19_17_47" id="wows1_6"/></li>
+		<li><a href="http://wowslider.net"><img src="data1/images/screenshot_20250324_195326_com.google.android.youtube.jpg" alt="html slider" title="Screenshot_20250324_195326_com.google.android.youtube" id="wows1_7"/></a></li>
+		<li><img src="data1/images/movimiento_nazionalista_mexicano_1.png" alt="Movimiento Nazionalista Mexicano 1" title="Movimiento Nazionalista Mexicano 1" id="wows1_8"/></li>
+	</ul></div>
+	<div class="ws_bullets"><div>
+		<a href="#" title="693_sin_titulo_20250320235254"><span><img src="data1/tooltips/693_sin_titulo_20250320235254.png" alt="693_sin_titulo_20250320235254"/>1</span></a>
+		<a href="#" title="692_sin_titulo_20250326092951"><span><img src="data1/tooltips/692_sin_titulo_20250326092951.png" alt="692_sin_titulo_20250326092951"/>2</span></a>
+		<a href="#" title="699_sin_titulo_20250324083516"><span><img src="data1/tooltips/699_sin_titulo_20250324083516.png" alt="699_sin_titulo_20250324083516"/>3</span></a>
+		<a href="#" title="703_sin_titulo_20250325084526"><span><img src="data1/tooltips/703_sin_titulo_20250325084526.png" alt="703_sin_titulo_20250325084526"/>4</span></a>
+		<a href="#" title="707_sin_titulo_20250327223117"><span><img src="data1/tooltips/707_sin_titulo_20250327223117.png" alt="707_sin_titulo_20250327223117"/>5</span></a>
+		<a href="#" title="707_sin_titulo_20250327224910"><span><img src="data1/tooltips/707_sin_titulo_20250327224910.png" alt="707_sin_titulo_20250327224910"/>6</span></a>
+		<a href="#" title="lun_24_03_2025_19_17_47"><span><img src="data1/tooltips/lun_24_03_2025_19_17_47.png" alt="lun_24_03_2025_19_17_47"/>7</span></a>
+		<a href="#" title="Screenshot_20250324_195326_com.google.android.youtube"><span><img src="data1/tooltips/screenshot_20250324_195326_com.google.android.youtube.jpg" alt="Screenshot_20250324_195326_com.google.android.youtube"/>8</span></a>
+		<a href="#" title="Movimiento Nazionalista Mexicano 1"><span><img src="data1/tooltips/movimiento_nazionalista_mexicano_1.png" alt="Movimiento Nazionalista Mexicano 1"/>9</span></a>
+	</div></div><div class="ws_script" style="position:absolute;left:-99%"><a href="http://wowslider.net">css image slider</a> by WOWSlider.com v9.0</div>
+<div class="ws_shadow"></div>
+</div>	
+<script type="text/javascript" src="engine1/wowslider.js"></script>
+<script type="text/javascript" src="engine1/script.js"></script>
+<!-- End WOWSlider.com BODY section -->
+ <br><br><br><br>
+
+<label class="manifiesto">Nuestro Manifiesto</label><br><br><br><br>
+<label>Hola, te invitamos a pertenecer al nazionalismo, una nueva ideología política donde se toman valores y principios del conservadurismo, totalitarismo, extrema derecha, nacionalismo etc, buscamos hacer un cambio en México con esta ideología.
+¿Que es el nazionalismo?
+El Nacionalismo mexicano es una nueva ideología política creada en 2025, basada en sistemas como el conservadurismo, extrema derecha, totalitarismo, nacionalismo.
+</label><br><br><br>
+
+
+
+
+<label>
+Por lo peculiar está ideología suele evitar otras como el comunismo, socialismo, marxismo en sus políticas, ya que para esta ideología son ineficientes y no funcionales.
+</label><br><br>
+
+
+<label>
+Se tienen creencias conservadoras, como la religión donde se defiende la iglesia católica, o la familia tradicional, y no apoyar comunidades como la feminista, woke, lgtb. Rechazamos la inmigración y mucho mas la gentrificacion, que es algo muy común en México, por ejemplo muchos grupos como los musulmanes o judíos son rechazados en nuestra ideología, por los peligrosos que son y las veces que an intentado atentar ante México.
+</label><br><br>
+
+<label><br><br>
+No somos xenófobos, México es un país multietnico como multicultural y multi población por lo cual en México viven muchas culturas, como las nativas, asiáticas e hispanas por lo cual aceptamos que México sea una nación multi étnica, y que no existe una raza mexicana, si no una cruza que forma al mexicano moderno, el aprendizaje de lenguas nativas en México deberían ser prioritarias que el inglés, que son muy importantes de la misma forma que el inglés, muchas de estas lenguas se están perdiendo y este movimiento busca apoyar a los nativos que temen a ser criticados por su idioma.
+</label><br><br>
+
+<label>
+El nazionalismo como lo dice su propio nombre su mayoría es de origen de esta misma, donde se incitan los valores de la patria, figuras patriotas, historia nacional, y ser autosustentable sin depender de otras naciones, un ejemplo seria apoyar empresas nacionales como jarritos en vez de coca cola, evitar monopolios extranjeros en México, además de impulsar la competencia comercial entre locales de la nación, tener una enseña mas nacionalista, dónde se muestre el país como una figura extremadamente importante y que se debe apoyar.
+Rechazo a partidos actuales, como sería el pt, morena, pri, pan etc, para el nazionalismo todos estos son corruptos e ineficientes, con mas defraudamientos que las promesas nunca dadas de estos gobiernos, mayor rechazo de morena, este se ve como un partido vendido por los grupos delictivos, y como manipulador de la mente de los civiles, con sus posturas de izquierda donde con un poco de dinero ganan miles. 
+¿Que cosas protegemos nosotros como nazionalistas?
+El nazionalismo mexicano defiende muchos sectores y le da relevancia mayor.
+</label><br><br>
+<label>
+Agricultura: la agricultura es altamente relevante se le da mucho mayor crédito a esta, como apoyos económicos, y de maquinaria para su trabajo, sin ellos el país no es nada, es la mayor fuente de alimento.
+</label><br><br>
+<label>
+Empresas nacionales: Se les dará mas tributo a las empresas nacionales mexicanas poniendo por detrás empresas extranjeras y con limitaciones que deben cumplir, además de siempre mostrar mas a las empresas mexicanas en cualquier sector.
+</label><br><br>
+<label>
+Competencia: negocios pequeños tendrían que vivir una competencia constante, para eso se busca apoyar financiaría mente a las que mas puedan ser con beneficios al país.
+</label><br><br>
+<label>
+Militarizacion: las fuerzas armadas son clave militarizacion: en el país, además de defender nuestra soberanía y de ataques terroristas como se viven en la actualidad deben ser apaciguados por una fuerza de verdad, tambien darle mas crédito a la armada y hacerla ver cómo nuestros amigos que nos defienden, al mismo tiempo de cambiar las formas policíacas como la policía municipal que a mostrado ser ineficiente y corrupta, formando una nuevas instituciones mas reguladas, con estudios y disciplina de verdad.
+conservadurismo: en el nazionalismo buscamos un país que tenga una vida mas tradicional y sin dejarse influir por cosas como la comunidad lgtb o el feminismo, aún que sin llegar a radicalizar, este conservadurismo no busca mantener a la población en un pasado constante, si no en una transformación que mantenga una vida cotidiana sin verse afectados por las normas sociales actuales donde se les da interés a personas con «influencia» en vez de dar tributo a los que mas aportan.
+</label><br><br>
+<label>
+Nacionalismo: el nacionalismo es la base del nazionalismo, tomando muchos de sus principios los mas importantes y mas influyentes.
+</label><br><br>
+<label>
+gentrificacion: regresar a sus Países natales a los ciudadanos que contribuyen a la gentrificacion y al desplazamiento de personas de sus viviendas, muchas de estas personas no pagan impuestos y afectan a los locales por lo cual nosotros contribuimos a ser deportados a su país de origen.
+</label><br><br>
+<label>
+inmigracion ilegal: México recibe mucha inmigración ilegal anualmente, el nazionalismo busca reforzar su deportación y cierre de fronteras a estos grupos de personas, por lo peculiar estos suelen querer ser mantenidos por el gobierno lo cual claramente rechazamos.
+</label>
+
+
+
+</body>
+</html>
